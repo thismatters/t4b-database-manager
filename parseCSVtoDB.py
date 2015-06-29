e02c0c7fa1bcc468649924bca7abc48b7a5964db
thismatters <thismatters@users.noreply.github.com>
2015-06-28 19:38:59 -0500

    parse script working!

--- parseCSVtoDB.py
+++ parseCSVtoDB.py
@@ -1,51 +1,84 @@
 from sys import argv
-
+from peewee import *
 # Using camelCase for functions
 # using split_by_underscores for variables
 
 script, filename = argv  
 # could stand to have some validation on what arguments are received
 
-# keys: columns from spreadsheet; values: (table, column in table).
-possible_data_columns = {'Last_Name': ('t4b', 'last_name'), 
-                         'First_Name_MI': ('t4b', 'first_name'), 
-                         'Phone1': ('t4b', 'phone'), 
-                         'Email1': ('t4b', 'email'),
-                         'Address': ('t4b', 'address'),
-                         'City': ('t4b', 'city'),
-                         'State': ('t4b', 'state'),
-                         'Zip': ('t4b', 'zipcode'),
-                         'Volunteer': ('t4b', 'will_volunteer'),
-                         'Pct': ('t4b', 'precinct'),
-                         'Capt': ('t4b', 'will_captain'),
-                         'DSA_Aus_Memb': ('affiliations', 'dsa_austin')}
+# establish database connection
+# put the real connection data in an unshared file
+t4b_db = MySQLDatabase('t4b', 
+                       host="localhost", 
+                       user="paul", 
+                       password="nothing")
 
+# keys: columns from spreadsheet; values: (table, column in table).
+possible_data_columns = {'Last_Name': ('person', 'last_name'), 
+                         'First_Name_MI': ('person', 'first_name_mi'), 
+                         'Phone1': ('person', 'phone'), 
+                         'Email1': ('person', 'email'),
+                         'Address': ('person', 'address'),
+                         'City': ('person', 'city'),
+                         'State': ('person', 'state'),
+                         'Zip': ('person', 'zipcode'),
+                         'Volunteer': ('person', 'will_volunteer'),
+                         'Pct': ('person', 'precinct'),
+                         'Capt': ('person', 'will_captain'),
+                         'DSA_Aus_Memb': ('affiliation', 'dsa_austin')}
+
+insert_priority = ['person', 'affiliation']
 
 columns_in_file = list()
-ordered_columns_for_insert = dict()
 def establishColumnOrder(header_row_data):
     for column_header in header_row_data:
         try:
             columns_in_file.append(possible_data_columns[column_header])
         except KeyError:
-            print "There is no database column corresponding to data column \'%s\'" % column_header
+            print "There is no database column corresponding to data column '%s'" % column_header
             print "All data in this column will be ignored!!!"
             columns_in_file.append(None)
-        else:
-            try:
-                ordered_columns_for_insert[columns_in_file[-1][0]] += (columns_in_file[-1][1],)
-            except KeyError:
-                ordered_columns_for_insert[columns_in_file[-1][0]] = (columns_in_file[-1][1],)
 
+class BaseModel(Model):
+    '''Establish the database which all other Models will inherit'''
+    class Meta:
+        database = t4b_db
 
-# open file
+class Person(BaseModel):
+    '''Establish the model for person table'''
+    person_id = PrimaryKeyField()
+    last_name = CharField(max_length=20)
+    first_name_mi = CharField(max_length=20)
+    # we need to decide which fields can be left empty!!
+    phone = CharField(max_length=11, null=True)
+    email = CharField(max_length=254, null=True)
+    address = CharField(max_length=100, null=True)
+    city = CharField(max_length=45, null=True)
+    state = CharField(max_length=2, null=True)
+    zipcode = CharField(max_length=10, null=True)
+    precinct = IntegerField(null=True)
+    will_volunteer = BooleanField(null=True)
+    will_captain = BooleanField(null=True)
+
+class Affiliation(BaseModel):
+    person = ForeignKeyField(Person)
+    # we might consider making this a many-to-one type of table where only
+    #    one affiliation is stored per row
+    dsa_austin = BooleanField()
+
+t4b_db.connect()
+t4b_db.create_tables([Person, Affiliation], safe=True)
+
 data_to_import = open(filename, 'r')
 
 first = True
 
 for row in data_to_import:
     row_data = row.rstrip().split(',')
-    inserts = dict()
+    insertable = dict()
+    for table in insert_priority:
+        insertable[table] = dict() 
+        # this could probably be made more efficient by copying a template
     if first:
         establishColumnOrder(row_data)
         first = False
@@ -56,10 +89,11 @@ for row in data_to_import:
             continue
         if entry.rstrip() is '':
             entry = None
-        # more processing and validation here
+        # more processing and validation here, 
+        # perhaps a data score that determines whether we keep the row
         table, column = column_data
-        try:
-            inserts[table] += (entry if entry is not None else NULL,)
-        except KeyError:
-            inserts[table] = (entry,)
-
+        insertable[table][column] = entry
+    
+    person = Person.create(**insertable['person'])
+    if insertable['affiliation']:
+        affiliation = Affiliation.create(person=person, **insertable['affiliation'])
