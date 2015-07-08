b073c8fac48ef2d9ccbdac5e042bc23ba130d378
thismatters <thismatters@users.noreply.github.com>
2015-07-08 13:43:32 -0500

    updated collision checking

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ The script can be called from the command line:
     python2.7 parseCSVtoDB.py csv_file.csv
 
 It will automatically create the tables (will **not** create the database) if they don't exist and populate them with the data in the CSV file. 
-The script does not require the columns to be in any order, but the column headings should not be changed. The script does **not** do any data validation or collision detection as of now. In the future I will implement duplicate checking on the email address and perhaps full name. 
+The script does not require the columns to be in any order, but the column headings should not be changed. The script does check for data collisions based on the email address; presently it reports collisions to a new file whose name is based on the input file but with "collisions_" prepended.  
 
 This script uses [peewee](http://peewee.readthedocs.org/en/latest/index.html) to interact with the mysql database. As such you will need to have peewee installed in your python distribution as well as [MySQLdb](http://stackoverflow.com/a/7461662/2754587).  
--- parseCSVtoDB.py
+++ parseCSVtoDB.py
@@ -8,10 +8,11 @@ script, filename = argv
 
 # establish database connection
 # put the real connection data in an unshared file
-t4b_db = MySQLDatabase('t4b', 
-                       host="localhost", 
-                       user="paul", 
-                       password="nothing")
+# t4b_db = MySQLDatabase('t4b', 
+#                        host="localhost", 
+#                        user="paul", 
+#                        password="nothing")
+t4b_db = SqliteDatabase('t4b.db')  # A local sqlite database for testing!
 
 # keys: columns from spreadsheet; values: (table, column in table).
 possible_data_columns = {'Last_Name': ('person', 'last_name'), 
@@ -29,26 +30,45 @@ possible_data_columns = {'Last_Name': ('person', 'last_name'),
 
 insert_priority = ['person', 'affiliation']
 
-columns_in_file = list()
+natural_column_ordering = ['first_name_mi', 'last_name', 'phone', 'email', 
+                           'address', 'city', 'state', 'zipcode', 
+                           'will_volunteer', 'precinct', 'will_captain']
+valid_columns_in_file = list()
+valid_column_headers_in_file = list()
+
 def establishColumnOrder(header_row_data):
     for column_header in header_row_data:
         try:
-            columns_in_file.append(possible_data_columns[column_header])
+            valid_columns_in_file.append(possible_data_columns[column_header])
         except KeyError:
             print "There is no database column corresponding to data column '%s'" % column_header
             print "All data in this column will be ignored!!!"
-            columns_in_file.append(None)
+            valid_columns_in_file.append(None)
+        else:
+            valid_column_headers_in_file.append(column_header) 
+
+def rowInFileColumnOrdering(row):
+    returnrow = ""
+    for column in valid_columns_in_file:
+        if column:
+            table, column_name = column
+            try: 
+                returnrow += (str(row[table][column_name]) + ", "  if row[table][column_name] else "")
+            except KeyError:
+                returnrow += ", "
+    return returnrow
 
 class BaseModel(Model):
-    '''Establish the database which all other Models will inherit'''
+    '''Establish the database from which all other Models will inherit'''
+
     class Meta:
         database = t4b_db
 
 class Person(BaseModel):
     '''Establish the model for person table'''
     person_id = PrimaryKeyField()
-    last_name = CharField(max_length=20)
-    first_name_mi = CharField(max_length=20)
+    last_name = CharField(max_length=20, null=True)
+    first_name_mi = CharField(max_length=20, null=True)
     # we need to decide which fields can be left empty!!
     phone = CharField(max_length=11, null=True)
     email = CharField(max_length=254, unique=True, null=True)
@@ -59,6 +79,24 @@ class Person(BaseModel):
     precinct = IntegerField(null=True)
     will_volunteer = BooleanField(null=True)
     will_captain = BooleanField(null=True)
+    
+    def __str__(self):
+        returnstring = "%s, " % self.person_id
+        try:
+            affiliations = Affiliation.get(Affiliation.person == self.person_id)
+        except Affiliation.DoesNotExist:
+            affiliations = None
+        for column in valid_columns_in_file:
+            if column is not None: 
+                if column[0] == "person":
+                    returnstring += (str(getattr(self, column[1])) + ", " if getattr(self, column[1]) else ", ")
+                if column[0] == "affiliation":
+                    if affiliations:
+                        returnstring += (str(getattr(affiliations, column[1])) + ", " if getattr(affiliations, column[1]) else ", ")
+                    else:
+                        returnstring += ", "
+
+        return returnstring
 
 class Affiliation(BaseModel):
     person = ForeignKeyField(Person)
@@ -70,7 +108,8 @@ t4b_db.connect()
 t4b_db.create_tables([Person, Affiliation], safe=True)
 
 data_to_import = open(filename, 'r')
-
+collisions_file_name = "collisions_%s" % filename
+collisions_file = open(collisions_file_name, 'w')
 first = True
 
 for row in data_to_import:
@@ -81,14 +120,18 @@ for row in data_to_import:
         # this could probably be made more efficient by copying a template
     if first:
         establishColumnOrder(row_data)
+        valid_column_headers_csv = ""
+        for column_header in valid_column_headers_in_file:
+            valid_column_headers_csv += "%s, " % column_header
+        collisions_file.write("Status, ID, %s \n" % valid_column_headers_csv)
         first = False
         continue
 
-    for entry, column_data in zip(row_data, columns_in_file):
+    for entry, column_data in zip(row_data, valid_columns_in_file):
         if column_data is None:
             continue
         if entry.rstrip() is '':
-            entry = None
+            continue
         # more processing and validation here, 
         # perhaps a data score that determines whether we keep the row
         table, column = column_data
@@ -97,8 +140,19 @@ for row in data_to_import:
         with t4b_db.transaction():
             person = Person.create(**insertable['person'])
     except IntegrityError:
-        print "Duplicate email detected: %s" (str(insertable['person']), )
+        duplicate_in_db = Person.get(Person.email == insertable['person']['email'])
+        # action, data = reconcileCollision(duplicate_in_db, insertable['person'])
+        # if action == "report":
+        formatted_new_duplicate = rowInFileColumnOrdering(insertable)
+        # print "Duplicate email detected: %s" % (formatted_new_duplicate, )
+        collisions_file.write("new, ,%s \n" % formatted_new_duplicate)
+        # print "Duplicate to: %s" % (duplicate_in_db, )
+        collisions_file.write("existing, %s \n" % duplicate_in_db)
         # put this duplicate in a file for review
+        # if action == "update":
+        #     ''''''
     else:
         if insertable['affiliation']:
             affiliation = Affiliation.create(person=person, **insertable['affiliation'])
+
+collisions_file.close() 
